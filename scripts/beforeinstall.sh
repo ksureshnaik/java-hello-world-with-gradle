1e5c2626f7fb77f3458145d001ba2d464b148395
ksureshnaik <101106073+ksureshnaik@users.noreply.github.com>
2022-05-17 12:08:57 +0530

    Update beforeinstall.sh

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install java-1.8.0-openjdk -y
-java -version
+yum -y install java-11-amazon-corretto-headless-11.0.13+8-2.amzn2.x86_64 log4j-cve-2021-44228-hotpatch
